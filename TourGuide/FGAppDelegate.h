9998e3fa7aaa8e1db672c6d378fb5cb5d618377b
Finn Gaida <finn.gaida@gmx.de>
2013-09-06 23:52:06 +0200

    Initial Commit

---
+++ TourGuide/FGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FGAppDelegate.h
+//  TourGuide
+//
+//  Created by Finn Gaida on 06.09.13.
+//  Copyright (c) 2013 Finn Gaida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TourGuide/FGViewController.h
@@ -0,0 +1,13 @@
+//
+//  FGViewController.h
+//  TourGuide
+//
+//  Created by Finn Gaida on 06.09.13.
+//  Copyright (c) 2013 Finn Gaida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FGViewController : UIViewController
+
+@end
